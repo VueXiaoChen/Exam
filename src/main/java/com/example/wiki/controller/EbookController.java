6bfb30057d06664d743521e583bea56a7115313c
陈 <455664949@qq.com>
2022-08-31 15:10:17 +0800

    添加公共消息体

--- src/main/java/com/example/wiki/controller/EbookController.java
+++ src/main/java/com/example/wiki/controller/EbookController.java
@@ -3,6 +3,7 @@ package com.example.wiki.controller;
 
 
 import com.example.wiki.domain.Ebook;
+import com.example.wiki.resp.CommonResp;
 import com.example.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,8 +17,22 @@ import java.util.List;
 public class EbookController {
     @Resource
     private EbookService ebookService;
+    //通过书名查询
+    @GetMapping("/find")
+    public CommonResp FindEbookByName(String name){
+        CommonResp<List<Ebook>> resp = new CommonResp<>();
+        List<Ebook> list= ebookService.FindEbookByName(name);
+        resp.setMessage("查询成功");
+        resp.setContent(list);
+        return resp;
+    }
+    //查询所有
     @GetMapping("/findall")
-    public List<Ebook> list(){
-        return ebookService.list();
+    public CommonResp list(){
+        CommonResp<List<Ebook>> resp = new CommonResp<>();
+        List<Ebook> list= ebookService.list();
+        resp.setMessage("获取成功");
+        resp.setContent(list);
+        return resp;
     }
 }
--- src/main/java/com/example/wiki/service/EbookService.java
+++ src/main/java/com/example/wiki/service/EbookService.java
@@ -1,6 +1,7 @@
 package com.example.wiki.service;
 
 import com.example.wiki.domain.Ebook;
+import com.example.wiki.domain.EbookExample;
 import com.example.wiki.mapper.EbookMapper;
 import org.springframework.stereotype.Service;
 
@@ -11,8 +12,15 @@ import java.util.List;
 public class EbookService {
     @Resource
     private EbookMapper ebookMapper;
-
     public List<Ebook> list(){
         return ebookMapper.selectByExample(null);
+    }
+    public List<Ebook> FindEbookByName(String name){
+        //下面两项是固定
+        EbookExample ebookExample =new EbookExample();
+        EbookExample.Criteria criteria= ebookExample.createCriteria();
+        //模糊查询
+        criteria.andNameLike("%"+name+"%");
+        return ebookMapper.selectByExample(ebookExample);
     }
 }
